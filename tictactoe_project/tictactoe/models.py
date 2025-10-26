560c5f543137f996eb592e3a4c314baa385bd872
kachkynova-artur <kachkynov_a@iuca.kg>
2025-10-26 15:10:36 +0600

    feat(models): add Game model and migrations

--- tictactoe_project/tictactoe/models.py
+++ tictactoe_project/tictactoe/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Game(models.Model):
+    player_x = models.CharField(max_length=50)
+    player_o = models.CharField(max_length=50)
+    winner = models.CharField(max_length=1, choices=[('X','X'),('O','O'),('D','Draw')], blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.player_x} vs {self.player_o}"
